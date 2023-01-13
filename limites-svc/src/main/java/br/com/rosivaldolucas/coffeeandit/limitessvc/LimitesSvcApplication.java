b52c8c73a26700a74ae8000bc23fb7be9e2ba8f8
Rosivaldo Lucas <rosivaldo@newsoft.online>
2023-01-13 13:08:40 -0300

    limites-svc - Adiciona servico de limites da aplicacao. Projeto ira ser responsavel por ser um microservice para limites de transacoes.

---
+++ limites-svc/src/main/java/br/com/rosivaldolucas/coffeeandit/limitessvc/LimitesSvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.rosivaldolucas.coffeeandit.limitessvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitesSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitesSvcApplication.class, args);
+	}
+
+}
---
+++ limites-svc/src/test/java/br/com/rosivaldolucas/coffeeandit/limitessvc/LimitesSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rosivaldolucas.coffeeandit.limitessvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitesSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
