7362d3f8787af6de49151b87df7f4c3ec89d3ca4
Rosivaldo Lucas <rosivaldo@newsoft.online>
2023-01-14 10:51:53 -0300

    transacoes-svc - Adiciona configuracoes iniciais do projeto.

---
+++ transacoes-svc/src/main/java/br/com/rosivaldolucas/coffeeandit/transacoessvc/TransacoesSvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.rosivaldolucas.coffeeandit.transacoessvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacoesSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacoesSvcApplication.class, args);
+	}
+
+}
---
+++ transacoes-svc/src/test/java/br/com/rosivaldolucas/coffeeandit/transacoessvc/TransacoesSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rosivaldolucas.coffeeandit.transacoessvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
